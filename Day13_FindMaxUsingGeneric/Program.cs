2ba3953e974a0c52324af7cd513eafab0c91795a
suraj <soorajkrpandit@gmail.com>
2022-05-06 14:15:32 +0530

    Add project files.

---
+++ Day13_FindMaxUsingGeneric/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13_FindMaxUsingGeneric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Find The Maximum Program");
+            int output = FindMaxNumber.MaximumIntegerNumber(70, 100, 29);
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }
+    }
+}
